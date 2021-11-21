fae3f3d9972bbf71ece0a2da10fda226a3707681
Emmanuel Lebeau <89812372+emmanuellebeau@users.noreply.github.com>
2021-11-21 15:55:02 -0500

    Add files via upload
    Corrected the if condition and replaced the for loop for the trials for a while one.

--- tasks/run_AB_lag1.py
+++ tasks/run_AB_lag1.py
@@ -11,24 +11,22 @@ import pyExperiment.runUtils as RU
 TODO:
     fix monitor
     fix dialogue box for getting responses
-    Do we keep the same pairs accros subject or use a different set each time?
 """
 
 taskdir = os.path.dirname(os.path.realpath(__file__))
 # define trial settings
 fix_time = 0.02
 img_dur = 0.02
-SOA = 0.1
-n_trials = 6 # per block
+SOA = 0.05
+n_trials = 126 # per block
 n_blocks = 3
-lag = [1, 2, 7]*(int(n_trials/n_blocks))
-np.random.shuffle(lag)
+#lag = [1, 2, 7]*(int(n_trials/n_blocks))
 t1_pos = 3
 t2_pos = 5
-RSVP_len = 15
+RSVP_len = 11
 n_masks = 24
 im_size = 8 # in degrees
-max_response_time = 2.5
+max_response_time = 0.01
 
 """
 Keys should be in order for your response menu
@@ -69,8 +67,9 @@ images = RU.load_images(os.path.join(taskdir,'stim'))
 n_images = len(images)
 
 # Define Pairs
-pairs = np.loadtxt('AB_pairs.txt', dtype = int, delimiter = ',')
+pairs = np.loadtxt('AB_pairs_test.txt', dtype = int, delimiter = ',')
 n_pairs = len(pairs)
+split_pairs = np.split(pairs, n_blocks)
 
 img_textures = []
 for i in range(n_images):
@@ -81,6 +80,7 @@ for i in range(n_images):
 # load masks
 masks = RU.createImageMasks(images, n_masks, 25)
 mask_textures = []
+
 for i in range(n_masks):
     progressBar(ab.win, i, n_masks,
                 load_txt=f'Loading masks')
@@ -94,69 +94,73 @@ for block in range(n_blocks):
                                        pos=(0, 0), height=0.5)
         params = {'class_obj': ab, 'obj_list': [info_message], 'responses': ['space']}
         drawAndWait(**params)
-        
-    #Randomize the lags of T2 for each trial
-    np.random.shuffle(lag)
-
-    #array from which the indexes will get picked and then deleted. Assure that
-    #no pair is shown twice in the same trial. 
-    pool_value = np.arange(len(pairs))
     
+    #array from which the indexes will get picked and then deleted. Assure that
+    #no pairs is shown twice in the same trial. 
+    pool_value = np.arange(n_trials)
+    last_T1 = 0
+    last_T2 = 0
     # Create all the trials for the block
-    for i in range(n_trials):
+    while len(pool_value) != 0:
         # Pick targets and create RSVP sequence
         index_pairs = rchoice(pool_value)
-        T1 = pairs[index_pairs][0]
-        T2 = pairs[index_pairs][1]
+        T1 = split_pairs[block][index_pairs][0]
+        T2 = split_pairs[block][index_pairs][1]
         pool_value = np.setdiff1d(pool_value, index_pairs)
-
+        
         # Randomly pick RSVP_len number of masks
         trial_sequence = [mask_textures[np.random.randint(n_masks)]
                                         for x in range(RSVP_len)]
 
-        # Replace the T1 and T2 positions with the targets
-        t1img = img_textures[T1]
-        t1img.name = f'T1 {T1}'
-        t2img = img_textures[T2]
-        t2img.name = f'T2 {T2}'
-        trial_sequence[t1_pos] = t1img
-        trial_sequence[t1_pos + int(lag[i])] = t2img
+        # Replace the T1 and T2 positions with the targets, make sure that no target is shown
+        #twice in a row.
+        if T1 != last_T1 and T1 != last_T2 and T2 != last_T1 and T2 != last_T2 or len( pool_value) == 1:
+            last_T1 = T1
+            last_T2 = T2
+            t1img = img_textures[T1]
+            t1img.name = f'T1 {T1}'
+            t2img = img_textures[T2]
+            t2img.name = f'T2 {T2}'
+            trial_sequence[t1_pos] = t1img
+            trial_sequence[t1_pos + int(pairs[index_pairs][2])] = t2img
 
-        # Make menu options
-        possible_menu_options = np.setdiff1d(range(n_images), [T1, T2])
+            # Make menu options
+            possible_menu_options = np.setdiff1d(range(n_images), [T1, T2])
 
-        T1_opt = np.append(rchoice(possible_menu_options, 1), T1)
-        np.random.shuffle(T1_opt)
+            T1_opt = np.append(rchoice(possible_menu_options, 1), T1)
+            np.random.shuffle(T1_opt)
 
-        T2_opt = np.append(rchoice(possible_menu_options, 1), T2)
-        np.random.shuffle(T2_opt)
+            T2_opt = np.append(rchoice(possible_menu_options, 1), T2)
+            np.random.shuffle(T2_opt)
 
-        # create text instances for menu
-        menu_txt = visual.TextStim(ab.win,
-                text='Which one was the first target', height=0.7)
-        menu_txt2 = visual.TextStim(ab.win,
-                text='Which one was the second target', height=0.7)
+            # create text instances for menu
+            menu_txt = visual.TextStim(ab.win,
+                    text='Which one was the first target', height=0.7)
+            menu_txt2 = visual.TextStim(ab.win,
+                    text='Which one was the second target', height=0.7)
 
-        T1_menu = [img_textures[x] for i, x in enumerate(T1_opt)]
-        T1_menu.append(menu_txt)
+            T1_menu = [img_textures[x] for i, x in enumerate(T1_opt)]
+            T1_menu.append(menu_txt)
 
-        T2_menu = [img_textures[x] for i, x in enumerate(T2_opt)]
-        T2_menu.append(menu_txt2)
+            T2_menu = [img_textures[x] for i, x in enumerate(T2_opt)]
+            T2_menu.append(menu_txt2)
 
-        # Add specifics to trial_dict
-        trial_dict['trial sequence'] = trial_sequence
-        trial_dict['trial type'] = lag[i]
-        trial_dict['T1'] = T1
-        trial_dict['T2'] = T2
-        trial_dict['T1 options'] = T1_opt
-        trial_dict['T2 options'] = T2_opt
-        trial_dict['T1 menu'] = T1_menu
-        trial_dict['T2 menu'] = T2_menu
-        trial_dict['T1 correct response'] = RU.getCorrectResponse(T1_opt,
-                                                                  T1, keys)
-        trial_dict['T2 correct response'] = RU.getCorrectResponse(T2_opt,
-                                                                  T2, keys)
-        ab.addTrial(trial_dict.copy())
+            # Add specifics to trial_dict
+            trial_dict['trial sequence'] = trial_sequence
+            trial_dict['trial type'] = split_pairs[block][index_pairs][2]
+            trial_dict['T1'] = T1
+            trial_dict['T2'] = T2
+            trial_dict['T1 options'] = T1_opt
+            trial_dict['T2 options'] = T2_opt
+            trial_dict['T1 menu'] = T1_menu
+            trial_dict['T2 menu'] = T2_menu
+            trial_dict['T1 correct response'] = RU.getCorrectResponse(T1_opt,
+                                                                        T1, keys)
+            trial_dict['T2 correct response'] = RU.getCorrectResponse(T2_opt,
+                                                                        T2, keys)
+            ab.addTrial(trial_dict.copy())
+        else:
+            pool_value = np.append(pool_value, index_pairs)
 
     if block == n_blocks-1: # if last block
         block_txt = f'End of run {ab.run}\npress space to continue'
